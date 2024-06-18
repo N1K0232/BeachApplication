6627fdb59a0ad481d755c577071498b8674d2ecb
N1K0232 <ns.nicolasilvestri@libero.it>
2024-06-18 14:12:27 +0200

    Add ListResult model

---
+++ src/BeachApplication.Shared/Collections/ListResult.cs
@@ -0,0 +1,3 @@
+﻿namespace BeachApplication.Shared.Collections;
+
+public record class ListResult<T>(IEnumerable<T>? Content, long TotalCount, int TotalPages, bool HasNextPage = false) where T : class;
