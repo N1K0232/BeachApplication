467c55b448eb4976183e0caa50bf680f9a141c70
N1K0232 <ns.nicolasilvestri@libero.it>
2024-06-19 12:47:17 +0200

    Add pagination extensions methods

---
+++ src/BeachApplication.DataAccessLayer/Extensions/QueryableExtensions.cs
@@ -0,0 +1,55 @@
+﻿using BeachApplication.DataAccessLayer.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeachApplication.DataAccessLayer.Extensions;
+
+public static class QueryableExtensions
+{
+    public static async Task<bool> HasNextPageAsync<T>(this IQueryable<T> source, int pageIndex, int itemsPerPage, CancellationToken cancellationToken = default) where T : BaseEntity
+    {
+        var hasItems = await source.AnyAsync(cancellationToken);
+        if (!hasItems)
+        {
+            return false;
+        }
+
+        var skip = pageIndex * itemsPerPage;
+        var take = itemsPerPage + 1;
+
+        var list = await source.Skip(skip).Take(take).ToListAsync(cancellationToken);
+        return list.Count > itemsPerPage;
+    }
+
+    public static async Task<int> TotalPagesAsync<T>(this IQueryable<T> source, int itemsPerPage, CancellationToken cancellationToken = default) where T : BaseEntity
+    {
+        var hasItems = await source.AnyAsync(cancellationToken);
+        if (!hasItems)
+        {
+            return 0;
+        }
+
+        var totalCount = await source.LongCountAsync(cancellationToken);
+        var totalPages = Convert.ToInt32(totalCount / itemsPerPage);
+
+        if ((totalPages % itemsPerPage) > 0)
+        {
+            totalPages++;
+        }
+
+        return totalPages;
+    }
+
+    public static async Task<IList<T>> ToListAsync<T>(this IQueryable<T> source, int pageIndex, int itemsPerPage, CancellationToken cancellationToken = default) where T : BaseEntity
+    {
+        var hasItems = await source.AnyAsync(cancellationToken);
+        if (!hasItems)
+        {
+            return Enumerable.Empty<T>().ToList();
+        }
+
+        var skip = pageIndex * itemsPerPage;
+        var take = itemsPerPage + 1;
+
+        return await source.Skip(skip).Take(take).ToListAsync(cancellationToken);
+    }
+}
