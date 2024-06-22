66b63add7a899d9032d0aa7fe6932437738a6b5c
N1K0232 <ns.nicolasilvestri@libero.it>
2024-06-22 12:16:12 +0200

    Add weather js client

--- src/BeachApplication/wwwroot/js/weather.js
+++ src/BeachApplication/wwwroot/js/weather.js
@@ -1,10 +1,20 @@
 ﻿function weather(language) {
     Alpine.data("weather", () => ({
-        city: '',
+        searchCity: '',
+        cityName: '',
+        condition: '',
+        conditionUrl: '',
+        conditionIconUrl: '',
+        temperature: 0.0,
         isBusy: false,
 
         reset: function () {
-            this.city = '';
+            this.searchCity = '';
+            this.cityName = '';
+            this.condition = '';
+            this.conditionUrl = '';
+            this.conditionIconUrl = '';
+            this.temperature = 0.0;
             this.isBusy = false;
         },
 
@@ -12,6 +22,29 @@
             this.isBusy = true;
 
             try {
+                var accessToken = window.localStorage.getItem('access_token');
+                var response = await fetch(`api/weatherforecast/${this.searchCity}`, {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Accept-Language": language,
+                        "Authorization": `Bearer ${accessToken}`
+                    }
+                });
+
+                var content = await response.json();
+                var errorMessage = GetErrorMessage(response.status, content);
+
+                if (errorMessage == null) {
+                    this.cityName = content.cityName;
+                    this.condition = content.condition;
+                    this.conditionUrl = content.conditionUrl;
+                    this.conditionIconUrl = content.conditionIconUrl;
+                    this.temperature = content.temperature;
+                }
+                else {
+                    alert(errorMessage);
+                }
             } catch (error) {
                 alert(error);
             }
