bb4a5654866194b3bdf8ff1ccfb24910e9f59663
N1K0232 <ns.nicolasilvestri@libero.it>
2024-06-22 11:56:42 +0200

    Add Dapper context

---
+++ src/BeachApplication.DataAccessLayer/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using BeachApplication.DataAccessLayer.Settings;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BeachApplication.DataAccessLayer;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddSqlContext(this IServiceCollection services, Action<SqlContextOptions> configuration)
+    {
+        ArgumentNullException.ThrowIfNull(services, nameof(services));
+        ArgumentNullException.ThrowIfNull(configuration, nameof(configuration));
+
+        var options = new SqlContextOptions();
+        configuration.Invoke(options);
+
+        services.AddScoped(_ => options);
+        services.AddScoped<ISqlContext, SqlContext>();
+
+        return services;
+    }
+}
