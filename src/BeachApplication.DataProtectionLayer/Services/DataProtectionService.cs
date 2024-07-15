76a7aa89ff88722fe0612976456b22f7d4f3600a
N1K0232 <ns.nicolasilvestri@libero.it>
2024-07-15 17:04:07 +0200

    Add CancellationToken parameter

--- src/BeachApplication.DataProtectionLayer/Services/DataProtectionService.cs
+++ src/BeachApplication.DataProtectionLayer/Services/DataProtectionService.cs
@@ -4,13 +4,13 @@ namespace BeachApplication.DataProtectionLayer.Services;
 
 public class DataProtectionService(IDataProtector protector) : IDataProtectionService
 {
-    public Task<string> ProtectAsync(string input)
+    public Task<string> ProtectAsync(string input, CancellationToken cancellationToken = default)
     {
         var result = protector.Protect(input);
         return Task.FromResult(result);
     }
 
-    public Task<string> UnprotectAsync(string input)
+    public Task<string> UnprotectAsync(string input, CancellationToken cancellationToken = default)
     {
         var result = protector.Unprotect(input);
         return Task.FromResult(result);
--- src/BeachApplication.DataProtectionLayer/Services/IDataProtectionService.cs
+++ src/BeachApplication.DataProtectionLayer/Services/IDataProtectionService.cs
@@ -3,6 +3,7 @@ namespace BeachApplication.DataProtectionLayer.Services;
 
 public interface IDataProtectionService
 {
-    Task<string> ProtectAsync(string input);
-    Task<string> UnprotectAsync(string input);
+    Task<string> ProtectAsync(string input, CancellationToken cancellationToken = default);
+
+    Task<string> UnprotectAsync(string input, CancellationToken cancellationToken = default);
 }
