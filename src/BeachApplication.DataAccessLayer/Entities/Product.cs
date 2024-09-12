2853b6ee768fd5e4c156d4ef3b9ec77d10ed55f2
N1K0232 <ns.nicolasilvestri@libero.it>
2024-09-12 21:29:39 +0200

    Update nullable reference types management

--- src/BeachApplication.DataAccessLayer/Entities/Product.cs
+++ src/BeachApplication.DataAccessLayer/Entities/Product.cs
@@ -14,7 +14,7 @@ public class Product : DeletableEntity
 
     public decimal Price { get; set; }
 
-    public virtual Category Category { get; set; } = null!;
+    public virtual Category Category { get; set; }
 
-    public virtual ICollection<OrderDetail>? OrderDetails { get; set; }
+    public virtual ICollection<OrderDetail> OrderDetails { get; set; }
 }
