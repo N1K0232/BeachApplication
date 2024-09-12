4c4e25ee90b5eaa2c6b3015961c5794cd6acea6c
N1K0232 <ns.nicolasilvestri@libero.it>
2024-09-12 21:34:30 +0200

    removed required

--- src/BeachApplication.DataAccessLayer/Entities/Product.cs
+++ src/BeachApplication.DataAccessLayer/Entities/Product.cs
@@ -6,9 +6,9 @@ public class Product : DeletableEntity
 {
     public Guid CategoryId { get; set; }
 
-    public required string Name { get; set; }
+    public string Name { get; set; }
 
-    public required string Description { get; set; }
+    public string Description { get; set; }
 
     public int? Quantity { get; set; }
 
