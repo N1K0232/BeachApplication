a4d0bc2f07ebc53642ae6a9c6cb794e1ecea3d1a
N1K0232 <ns.nicolasilvestri@libero.it>
2024-10-06 21:45:14 +0200

    Update identity and authentication

--- src/BeachApplication/Endpoints/IdentityEndpoints.cs
+++ src/BeachApplication/Endpoints/IdentityEndpoints.cs
@@ -1,12 +1,14 @@
 ﻿using System.Security.Claims;
-using BeachApplication.BusinessLayer.Services.Interfaces;
-using BeachApplication.DataAccessLayer.Extensions;
+using AutoMapper;
+using BeachApplication.DataAccessLayer;
+using BeachApplication.DataAccessLayer.Entities.Identity;
 using BeachApplication.Shared.Models;
 using BeachApplication.Shared.Models.Requests;
 using BeachApplication.Shared.Models.Responses;
-using Microsoft.AspNetCore.Http.HttpResults;
+using FluentEmail.Core;
+using Microsoft.AspNetCore.Identity;
 using MinimalHelpers.Routing;
-using OperationResults.AspNetCore.Http;
+using SimpleAuthentication.JwtBearer;
 
 namespace BeachApplication.Endpoints;
 
@@ -16,19 +18,12 @@ public class IdentityEndpoints : IEndpointRouteHandlerBuilder
     {
         var identityApiGroup = endpoints.MapGroup("/api/auth");
 
-        identityApiGroup.MapGet("/me", GetMe)
+        identityApiGroup.MapGet("/profile", GetMeAsync)
             .Produces<User>(StatusCodes.Status200OK)
             .Produces(StatusCodes.Status401Unauthorized)
             .Produces(StatusCodes.Status403Forbidden)
             .RequireAuthorization("UserActive")
-            .WithName("getme")
-            .WithOpenApi();
-
-        identityApiGroup.MapGet("/qrcode", GetQrCodeAsync)
-            .Produces<FileContentHttpResult>(StatusCodes.Status200OK)
-            .Produces(StatusCodes.Status400BadRequest)
-            .AllowAnonymous()
-            .WithName("qrcode")
+            .WithName("profile")
             .WithOpenApi();
 
         identityApiGroup.MapPost("/login", LoginAsync)
@@ -60,13 +55,6 @@ public class IdentityEndpoints : IEndpointRouteHandlerBuilder
             .WithName("updatepassword")
             .WithOpenApi();
 
-        identityApiGroup.MapPost("/validatetwofactor", ValidateTwoFactorAsync)
-            .Produces<AuthResponse>(StatusCodes.Status200OK)
-            .Produces(StatusCodes.Status400BadRequest)
-            .AllowAnonymous()
-            .WithName("validatetwofactor")
-            .WithOpenApi();
-
         identityApiGroup.MapGet("/verifyemail", VerifyEmailAsync)
             .Produces(StatusCodes.Status204NoContent)
             .Produces(StatusCodes.Status400BadRequest)
@@ -76,58 +64,130 @@ public class IdentityEndpoints : IEndpointRouteHandlerBuilder
             .WithOpenApi();
     }
 
-    private static Results<Ok<User>, UnauthorizedHttpResult, ForbidHttpResult> GetMe(ClaimsPrincipal principal)
+    private static async Task<IResult> GetMeAsync(UserManager<ApplicationUser> userManager, IMapper mapper, ClaimsPrincipal principal)
     {
-        var user = new User
-        {
-            Id = principal.GetId(),
-            FirstName = principal.GetClaimValue(ClaimTypes.GivenName),
-            LastName = principal.GetClaimValue(ClaimTypes.Surname),
-            Email = principal.GetEmail()
-        };
+        var dbUser = await userManager.GetUserAsync(principal);
+        var user = mapper.Map<User>(dbUser);
 
         return TypedResults.Ok(user);
     }
 
-    private static async Task<IResult> GetQrCodeAsync(IIdentityService identityService, string token, HttpContext httpContext)
+    private static async Task<IResult> LoginAsync(SignInManager<ApplicationUser> signInManager, IJwtBearerService jwtBearerService, LoginRequest request)
     {
-        var result = await identityService.GetQrCodeAsync(token);
-        return httpContext.CreateResponse(result);
+        var user = await signInManager.UserManager.FindByEmailAsync(request.Email);
+        var result = await signInManager.PasswordSignInAsync(user, request.Password, request.IsPersistent, true);
+
+        if (!result.Succeeded)
+        {
+            var isEmailConfirmed = await signInManager.UserManager.IsEmailConfirmedAsync(user);
+            if (!isEmailConfirmed)
+            {
+                return TypedResults.BadRequest("You have to verify your account first");
+            }
+
+            var isLockedOut = await signInManager.UserManager.IsLockedOutAsync(user);
+            if (isLockedOut)
+            {
+                return TypedResults.BadRequest("Your account is locked out");
+            }
+
+            await signInManager.UserManager.AccessFailedAsync(user);
+            return TypedResults.BadRequest("Invalid username or password");
+        }
+
+        var userRoles = await signInManager.UserManager.GetRolesAsync(user);
+        await signInManager.UserManager.UpdateSecurityStampAsync(user);
+
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.GivenName, user.FirstName),
+            new Claim(ClaimTypes.Surname, user.LastName ?? string.Empty),
+            new Claim(ClaimTypes.Email, user.Email),
+            new Claim(ClaimTypes.SerialNumber, user.SecurityStamp)
+        }.Union(userRoles.Select(role => new Claim(ClaimTypes.Role, role)));
+
+        var accessToken = await jwtBearerService.CreateTokenAsync(user.UserName, claims.ToList());
+        return TypedResults.Ok(new AuthResponse(accessToken));
     }
 
-    private static async Task<IResult> LoginAsync(IIdentityService identityService, LoginRequest request, HttpContext httpContext)
+    private static async Task<IResult> RegisterAsync(UserManager<ApplicationUser> userManager, IFluentEmail fluentEmail, LinkGenerator linkGenerator, HttpContext httpContext, RegisterRequest request)
     {
-        var result = await identityService.LoginAsync(request);
-        return httpContext.CreateResponse(result);
+        var user = new ApplicationUser
+        {
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            Email = request.Email,
+            UserName = request.Email,
+            PhoneNumber = request.PhoneNumber
+        };
+
+        var result = await userManager.CreateAsync(user, request.Password);
+        if (!result.Succeeded)
+        {
+            return TypedResults.BadRequest(result.Errors.Select(e => e.Description));
+        }
+
+        var token = await userManager.GenerateEmailConfirmationTokenAsync(user);
+        var values = new RouteValueDictionary
+        {
+            ["userId"] = user.Id,
+            ["token"] = token
+        };
+
+        var endpoint = linkGenerator.GetUriByRouteValues(httpContext, "verifyemail", values, httpContext.Request.Scheme);
+        var sendResult = await fluentEmail.To(user.Email).Subject("Confirm your email")
+            .Body($"Please confirm your email by clicking this link: <a href='{endpoint}'>Confirm Email</a>", true)
+            .SendAsync();
+
+        if (!sendResult.Successful)
+        {
+            return TypedResults.BadRequest(sendResult.ErrorMessages);
+        }
+
+        return TypedResults.Created();
     }
 
-    private static async Task<IResult> RegisterAsync(IIdentityService identityService, RegisterRequest request, HttpContext httpContext)
+    private static async Task<IResult> ResetPasswordAsync(UserManager<ApplicationUser> userManager, ResetPasswordRequest request)
     {
-        var result = await identityService.RegisterAsync(request);
-        return httpContext.CreateResponse(result, StatusCodes.Status201Created);
+        var user = await userManager.FindByEmailAsync(request.Email);
+        if (user is null)
+        {
+            return TypedResults.BadRequest("Invalid email address");
+        }
+
+        var token = await userManager.GeneratePasswordResetTokenAsync(user);
+        return TypedResults.Ok(new ResetPasswordResponse(token));
     }
 
-    private static async Task<IResult> ResetPasswordAsync(IIdentityService identityService, ResetPasswordRequest request, HttpContext httpContext)
+    private static async Task<IResult> UpdatePasswordAsync(UserManager<ApplicationUser> userManager, ChangePasswordRequest request)
     {
-        var result = await identityService.ResetPasswordAsync(request);
-        return httpContext.CreateResponse(result);
+        var user = await userManager.FindByEmailAsync(request.Email);
+        if (user is null)
+        {
+            return TypedResults.BadRequest("Invalid email address");
+        }
+
+        var result = await userManager.ResetPasswordAsync(user, request.Token, request.Password);
+        if (!result.Succeeded)
+        {
+            return TypedResults.BadRequest(result.Errors.Select(e => e.Description));
+        }
+
+        return TypedResults.NoContent();
     }
 
-    private static async Task<IResult> UpdatePasswordAsync(IIdentityService identityService, ChangePasswordRequest request, HttpContext httpContext)
+    private static async Task<IResult> VerifyEmailAsync(UserManager<ApplicationUser> userManager, string userId, string token)
     {
-        var result = await identityService.UpdatePasswordAsync(request);
-        return httpContext.CreateResponse(result);
-    }
+        var user = await userManager.FindByIdAsync(userId);
+        var result = await userManager.ConfirmEmailAsync(user, token);
 
-    private static async Task<IResult> ValidateTwoFactorAsync(IIdentityService identityService, TwoFactorRequest request, HttpContext httpContext)
-    {
-        var result = await identityService.ValidateTwoFactorAsync(request);
-        return httpContext.CreateResponse(result);
-    }
+        if (result.Succeeded)
+        {
+            await userManager.AddToRoleAsync(user, RoleNames.User);
+            return TypedResults.NoContent();
+        }
 
-    private static async Task<IResult> VerifyEmailAsync(IIdentityService identityService, Guid userId, string token, HttpContext httpContext)
-    {
-        var result = await identityService.VerifyEmailAsync(userId, token);
-        return httpContext.CreateResponse(result);
+        return TypedResults.BadRequest("Couldn't verify your email");
     }
 }
