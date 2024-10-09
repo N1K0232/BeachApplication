b51c38bfe27972c5b08e529438fc9ebb1fb899aa
N1K0232 <ns.nicolasilvestri@libero.it>
2024-10-09 23:34:29 +0200

    refactoring

--- src/BeachApplication/wwwroot/js/auth.js
+++ src/BeachApplication/wwwroot/js/auth.js
@@ -1,6 +1,6 @@
 ﻿function auth(language) {
     Alpine.data("auth", () => ({
-        id: '',
+
         firstName: '',
         lastName: '',
         email: '',
@@ -11,7 +11,6 @@
         passwordErrorMessage: '',
 
         clear: function () {
-            this.id = '';
             this.firstName = '';
             this.lastName = '';
             this.email = '';
@@ -31,24 +30,10 @@
             this.isBusy = true;
 
             try {
-                var request = {
-                    email: this.email,
-                    password: this.password,
-                    isPersistent: this.isPersistent
-                };
+                const response = await loginAsync(this.email, this.password, this.isPersistent, language);
+                const content = await response.json();
 
-                var response = await fetch('/api/auth/login', {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                        "Accept-Language": language
-                    },
-                    body: JSON.stringify(request)
-                });
-
-                var content = await response.json();
-                var errorMessage = GetErrorMessage(response.status, content);
-
+                const errorMessage = GetErrorMessage(response.status, content);
                 if (errorMessage == null) {
                     window.localStorage.setItem('access_token', content.accessToken);
                     window.location.href = '/';
@@ -64,26 +49,15 @@
             }
         },
 
-        me: async function () {
+        loadProfile: async function () {
             this.isBusy = true;
 
             try {
-                var accessToken = GetAccessToken();
-                var response = await fetch('api/me', {
-                    method: "GET",
-                    headers: {
-                        "Content-type": "application/json",
-                        "Accept-Language": language,
-                        "Authorization": `Bearer ${accessToken}`
-                    }
-                });
+                const response = await loadProfileAsync(GetAccessToken(), language);
+                const content = await response.json();
 
-                var content = await response.json();
-                var errorMessage = GetErrorMessage(response.status, content);
-
+                const errorMessage = GetErrorMessage(response.status, content);
                 if (errorMessage == null) {
-                    this.fullName = `${content.firstName} ${content.lastName}`;
-                    this.email = content.email;
                 }
                 else {
                     alert(errorMessage);
@@ -106,36 +80,12 @@
                 return;
             }
 
-            const firstName = this.firstName;
-            const lastName = this.lastName;
-            const email = this.email;
-            const password = this.password;
-
-            this.firstName = '';
-            this.lastName = '';
-            this.email = '';
-            this.password = '';
-
             try {
-                var request = {
-                    firstName: firstName,
-                    lastName: lastName,
-                    email: email,
-                    password: password
-                };
 
-                var response = await fetch('/api/auth/register', {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                        "Accept-Language": language
-                    },
-                    body: JSON.stringify(request)
-                });
+                const response = await registerAsync(this.firstName, this.lastName, this.email, this.password, language);
+                const content = await response.json();
 
-                var content = await response.json();
-                var errorMessage = GetErrorMessage(response.status, content);
-
+                const errorMessage = GetErrorMessage(response.status, content);
                 if (errorMessage == null) {
                     window.location.href = '/Accounts/Login';
                 }
@@ -154,19 +104,10 @@
             this.isBusy = true;
 
             try {
-                var request = { email: this.email };
-                var response = await fetch('/api/auth/resetpassword', {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                        "Accept-Language": language
-                    },
-                    body: JSON.stringify(request)
-                });
+                const response = await resetPasswordAsync(this.email, language);
+                const content = await response.json();
 
-                var content = await response.json();
-                var errorMessage = GetErrorMessage(response.status, content);
-
+                const errorMessage = GetErrorMessage(response.status, content);
                 if (errorMessage == null) {
                     window.localStorage.setItem('reset_password_token', content.token);
                 }
@@ -191,32 +132,12 @@
                 return;
             }
 
-            const email = this.email;
-            const password = this.password;
-
-            this.email = '';
-            this.password = '';
-            this.confirmPassword = '';
-
             try {
-                var token = window.localStorage.getItem('reset_password_token');
-                var request = {
-                    email: email,
-                    password: password,
-                    token: token
-                };
+                const token = window.localStorage.getItem('reset_password_token');
+                const response = await updatePasswordAsync(this.email, token, language);
 
-                var response = await fetch('/api/auth/updatepassword', {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                        "Accept-Language": language
-                    },
-                    body: JSON.stringify(request)
-                });
-
-                var content = await response.json();
-                var errorMessage = GetErrorMessage(response.status, content);
+                const content = await response.json();
+                const errorMessage = GetErrorMessage(response.status, content);
 
                 if (errorMessage == null) {
                     window.location.href = '/';
@@ -246,4 +167,81 @@
 
 function checkPassword(password, confirmPassword) {
     return confirmPassword.trim().toUpperCase() === password.trim().toUpperCase();
+}
+
+async function loadProfileAsync(accessToken, language) {
+    const response = await fetch('/api/auth/profile', {
+        method: "GET",
+        headers: {
+            "Content-type": "application/json",
+            "Accept-Language": language,
+            "Authorization": `Bearer ${accessToken}`
+        }
+    });
+
+    return response;
+}
+
+async function loginAsync(email, password, isPersistent, language) {
+
+    const request = { email: email, password: password, isPersistent: isPersistent };
+    const response = await fetch('/api/auth/login', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept-Language": language
+        },
+        body: JSON.stringify(request)
+    });
+
+    return response;
+}
+
+async function registerAsync(firstName, lastName, email, password, language) {
+
+    const request = { firstName: firstName, lastName: lastName, email: email, password: password };
+    const response = await fetch('/api/auth/register', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept-Language": language
+        },
+        body: JSON.stringify(request)
+    });
+
+    return response;
+}
+
+async function resetPasswordAsync(email, language) {
+    const request = { email: email };
+    const response = await fetch('/api/auth/resetpassword', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept-Language": language
+        },
+        body: JSON.stringify(request)
+    });
+
+    return response;
+}
+
+async function updatePasswordAsync(email, token, language) {
+
+    const request = {
+        email: email,
+        password: password,
+        token: token
+    };
+
+    const response = await fetch('/api/auth/updatepassword', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept-Language": language
+        },
+        body: JSON.stringify(request)
+    });
+
+    return response;
 }
