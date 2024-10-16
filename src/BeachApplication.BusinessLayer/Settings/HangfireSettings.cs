ca1c17b0df4a2c9fb5e8088adbfb98104f31eda4
N1K0232 <ns.nicolasilvestri@libero.it>
2024-10-16 17:41:36 +0200

    Add Hangfire settings

---
+++ src/BeachApplication.BusinessLayer/Settings/HangfireSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace BeachApplication.BusinessLayer.Settings;
+
+public class HangfireSettings
+{
+    public bool Enabled { get; init; }
+
+    public string UserName { get; init; }
+
+    public string Password { get; init; }
+}
