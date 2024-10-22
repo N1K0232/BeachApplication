9116f2fe5e3cd9dacd00e8848fa232d231fd3991
N1K0232 <ns.nicolasilvestri@libero.it>
2024-10-22 17:21:12 +0200

    Add Products management

---
+++ src/BeachApplication.BusinessLayer/BackgroundServices/ProductsManagerBackgroundJob.cs
@@ -0,0 +1,24 @@
+﻿using BeachApplication.DataAccessLayer;
+using BeachApplication.DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using Quartz;
+
+namespace BeachApplication.BusinessLayer.BackgroundServices;
+
+public class ProductsManagerBackgroundJob(IApplicationDbContext db) : IJob
+{
+    public async Task Execute(IJobExecutionContext context)
+    {
+        var products = await db.GetData<Product>().ToListAsync(context.CancellationToken);
+        foreach (var product in products)
+        {
+            var item = await db.GetData<CartItem>(trackingChanges: true).FirstOrDefaultAsync(c => c.ProductId == product.Id, context.CancellationToken);
+            if (item is not null && product.Quantity is not null && product.Quantity < item.Quantity)
+            {
+                item.Quantity = product.Quantity.Value;
+            }
+        }
+
+        await db.SaveAsync();
+    }
+}
