23b7ab761aed7ad8826f6606e1d9b65648290b3d
N1K0232 <ns.nicolasilvestri@libero.it>
2024-10-22 17:21:27 +0200

    refactoring for Quartz service

---
+++ src/BeachApplication/Extensions/QuartzExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Quartz;
+
+namespace BeachApplication.Extensions;
+
+public static class QuartzExtensions
+{
+    public static IServiceCollectionQuartzConfigurator AddScheduledJob<TJob>(this IServiceCollectionQuartzConfigurator configurator, string cronSchedule) where TJob : class, IJob
+    {
+        var jobName = typeof(TJob).Name;
+        configurator.AddJob<TJob>(options => options.WithIdentity(jobName));
+
+        configurator.AddTrigger(options =>
+        {
+            options.ForJob(jobName)
+                .WithIdentity($"{jobName}-trigger")
+                .WithCronSchedule(cronSchedule);
+        });
+
+        return configurator;
+    }
+}
